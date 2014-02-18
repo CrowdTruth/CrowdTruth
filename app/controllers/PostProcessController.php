160e3b4b0a16972fb02f1f26d89d8d70f982e5bd
Jelle <j.van.der.ploeg.88@gmail.com>
2014-02-18 14:07:44 +0100

    Included angular as tree in application.js, set up annotation of angular variables with [[variable]] in order for it not to conflict with blade. Angular application is setup as dataRetrieval, now starting with controller for result view

--- app/controllers/PostProcessController.php
+++ app/controllers/PostProcessController.php
@@ -13,18 +13,23 @@ class PostProcessController extends BaseController {
 	//Change JobConfiguration into JobConfiguration
 	public function getListview() {
 		// Uncomment for mock entry for listview
-		$ct = JobConfiguration::fromJSON(base_path() . '/public/templates/relation_direction/relation_direction_multiple.json');
-		$ct->createdBy = 'Oana';
-		$ct->name = 'Mock-object #4';
-		$ct->judgmentsPerUnit = 12;
-		$ct->unitsPerTask = 8;
-		$ct->reward = 0.02;
-		$ct->platform = array('CF');
-		$ct->flaggedWorkers = 3;
-		$ct->template = "Relation Direction";
-		$ct->save();
+		// $ct = JobConfiguration::fromJSON(base_path() . '/public/templates/relation_direction/relation_direction_multiple.json');
+		// $ct->createdBy = 'Oana';
+		// $ct->name = 'Mock-object #4';
+		// $ct->judgmentsPerUnit = 12;
+		// $ct->unitsPerTask = 8;
+		// $ct->reward = 0.02;
+		// $ct->platform = array('CF');
+		// $ct->flaggedWorkers = 3;
+		// $ct->template = "Relation Direction";
+		// $ct->save();
+
 		$jobConfigurations = JobConfiguration::orderBy('judgmentsPerUnit','asc')->paginate(15);
 		return View::make('postprocess.listview')->with('jobConfigurations', $jobConfigurations);
+	}
+
+	public function getTableview(){
+		return View::make('postprocess.tableview');
 	}
 
 	/** via routes this method for sorting is called
--- app/views/postprocess/tableview.blade.php
+++ app/views/postprocess/tableview.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.default')
 
 @section('content')
-	<div class="container" ng-app>
-		
+	<div class="container" ng-app="DataRetrieval">
+		<div ng-controller="ResultCtrl">
+			[[totalJobs]]
+		</div>						
 	</div>
 
 @section('end_javascript')
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.13/angular.min.js">
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.13/angular.min.js"></script>
+
