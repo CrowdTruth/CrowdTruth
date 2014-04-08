8f162ad7384ffcf323992d45de2182482155983b
tacri <tatiana.cristea@gmail.com>
2014-04-08 15:56:31 +0000

    Visualization information, need new vagrant for working

---
+++ app/assets/javascripts/visualizations/moduleJobVis.js
@@ -0,0 +1,1 @@
+var app = angular.module("jobanalytics", ['nvd3ChartDirectives', 'ngResource']);
--- app/routes.php
+++ app/routes.php
@@ -9,6 +9,8 @@ Route::group(array('before' => 'auth'), function()
 {
 	Route::get('/', function()
 	{
+        //$result = \MongoDB\Entity::where('documentType', 'job')->whereIn('softwareAgent_id', ['cf', 'amt'])->count();
+        //return $result;
 	    return Redirect::to('home');
 	});
 
@@ -23,6 +25,7 @@ Route::group(array('before' => 'auth'), function()
 	Route::controller('process', 'ProcessController');
 	Route::controller('jobs', 'JobsController');
 	Route::controller('workers', 'WorkersController');
+    Route::controller('analyze','AnalyticsController');
 	
 });
 
