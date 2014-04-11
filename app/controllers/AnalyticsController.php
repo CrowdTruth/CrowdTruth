db78ce828abdbe879378de8c1f9597cdf16b7f89
khamkham <k.khamkham+github@gmail.com>
2014-04-11 11:48:54 +0200

    Update AnalyticsController.php

--- app/controllers/AnalyticsController.php
+++ app/controllers/AnalyticsController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 use crowdwatson\MechanicalTurkService;
@@ -43,49 +42,3 @@ class AnalyticsController extends BaseController {
         return Redirect::to('jobs/listview');
     }
 }
-=======
-<?php
-
-use crowdwatson\MechanicalTurkService;
-
-use \MongoDB\Repository as Repository;
-use \MongoDB\Entity as Entity;
-use \MongoDB\Activity as Activity;
-use \MongoDB\SoftwareAgent as SoftwareAgent;
-use \MongoDB\CrowdAgent as CrowdAgent;
-
-class AnalyticsController extends BaseController {
-
-    public $restful = true;
-    public static $colorList = array( '#FF7F50','#659CEF', '#FFDAB9 ', '#8FBC8F', '#FFA07A', '#B0C4DE', '#CD5C5C', '#9ACD32',  '#DAA520');
-    protected $repository;
-
-    public function __construct(Repository $repository){
-        $this->repository = $repository;
-    }
-
-    public function anyView(){
-
-        $jobArray = explode(',', Input::get('jobs'));
-        $colors = array();
-        $jobsInfo = array();
-
-        for ($iter = 0; $iter < count($jobArray); ++$iter) {
-
-            $jobID = $jobArray[$iter];
-            $jobsInfo[$jobID] = \MongoDB\Entity::find($jobID);
-            $color = AnalyticsController::$colorList[$iter%count(AnalyticsController::$colorList)];
-            $jobsInfo[$jobID]['color'] = $color;
-            $colors[$jobID] = $color;
-        }
-
-
-        return View::make('analytics.jobview')->with('jobConfigurations', $jobsInfo)
-            ->with('jobIDs', $jobArray)
-            ->with('jobColors',$colors);
-    }
-    public function getIndex() {
-        return Redirect::to('jobs/listview');
-    }
-}
->>>>>>> 373a996f28c09ac33cb9afd1d59c621e5cca2fd5
