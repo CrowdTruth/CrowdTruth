3261b4f8d9744745f0a4b0f57eb13d51a3d5d943
arner <arner@users.noreply.github.com>
2014-04-15 16:27:38 +0200

    Update listview.blade.php

--- app/views/jobs/listview.blade.php
+++ app/views/jobs/listview.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('head')
-<link rel="stylesheet" type="text/css" href="/custom_assets/custom.css"></link>
+<link rel="stylesheet" type="text/css" href="/custom_assets/custom.css" />
 <script type="text/javascript" src="/custom_assets/angular-moment.js"></script>
 <script type="text/javascript" src="/custom_assets/moment.js"></script>
 <script type="text/javascript" src="/custom_assets/crowdwatson.js"></script>
