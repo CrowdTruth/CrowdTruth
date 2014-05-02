ca67cb3bfd67e77d86116bbe48789ae8c52f709f
khamkham <root@precise32.(none)>
2014-05-02 12:32:18 +0000

    routes

--- app/routes.php
+++ app/routes.php
@@ -8,39 +8,6 @@
 Route::group(array('before' => 'auth'), function()
 {
 
-	// \DB::collection('entities')->whereIn('documentType', ['twrex-structured-sentence'])->limit(1)->push('tags', ['unit']);
-
-	// return \MongoDB\Entity::where('documentType', 'twrex-structured-sentence')->limit(1)->get();
-
-	// \DB::collection('entities')->whereIn('documentType', ['twrex', 'twrex-structured-sentence', 'fullvideo'])->push('tags', ['unit']);
-
-
-
-	// set_time_limit(1200);
-
- //    \Session::flash('rawArray', 1);
- //    $db = \DB::getMongoDB();
- //    $db = $db->temp;
-    
- //    $result = \MongoDB\Entity::where('documentType', 'twrex-structured-sentence')->with('wasAttributedToUserAgent', 'wasGeneratedBy')->get()->toArray();
-
- //    array_push($result, [
- //        "_id" => "unitsCache",
- //        "created_at" => new \MongoDate(time()),
- //    ]);
-
- //    try {
- //        $db->batchInsert(
- //            $result,
- //            array('continueOnError' => true)
- //        );             
- //    } catch (Exception $e) {
- //    // ContinueOnError will still throw an exception on duplication, even though it continues, so we just move on.
- //    }
-
- //    \Session::forget('rawArray');
-
- //    dd('done');
 
 	Route::controller('media/preprocess/fullvideo', 'preprocess\FullvideoController');
 	Route::controller('media/preprocess/twrex', 'preprocess\TwrexController');
@@ -60,16 +27,6 @@ Route::group(array('before' => 'auth'), function()
 
 Route::get('/', function()
 {
-
-	// \MongoDB\Temp::whereIn('_id', ['mainSearchFilters', 'jobCache'])->forceDelete();
-
-	// dd('done');
-
-	\DB::collection('entities')->unset('tags');
-	\DB::collection('entities')->whereIn('documentType', ['twrex-structured-sentence', 'twrex'])->push('tags', 'unit', true);
-
-	// return \MongoDB\Entity::where('documentType', 'twrex-structured-sentence')->limit(1)->get();
-
     return Redirect::to('home');
 });
 
