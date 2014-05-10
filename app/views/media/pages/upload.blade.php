cbe6bcb09194a59991605e392002e452a83b699c
Oana Inel <oana.inel@gmail.com>
2014-05-10 20:46:49 +0000

    individual page for videos

--- app/views/media/pages/upload.blade.php
+++ app/views/media/pages/upload.blade.php
@@ -175,6 +175,7 @@
 							'numberVideos' 			: $('.onlineForm input[name=numberVideos]').val()
 						};
 
+						
 						// process the form
 						$.ajax({
 							type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
@@ -191,9 +192,9 @@
 								// here we will handle errors and validation messages
 							});
 
-							//setTimeout( function(){
+							setTimeout( function(){
 					location.href = "{{ URL::to('media/upload') }}";						
-							//}, 2000);
+							}, 2000);
 
 
 
