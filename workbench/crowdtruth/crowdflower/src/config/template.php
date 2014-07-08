e3efba35c85751cc2a05283f177fdf7f7296b54e
lr292358 <lukasz21212121@wp.pl>
2014-07-08 14:18:21 +0000

    Towards job2, part4 (lukasz)

--- workbench/crowdtruth/crowdflower/src/config/template.php
+++ workbench/crowdtruth/crowdflower/src/config/template.php
@@ -2,7 +2,7 @@
 
 return array(
 
-	'apikey' => 'H_MX6QhTNBmSE-zdsqd1',
+	'apikey' => '',
 	'webhookuri' => Request::root() . '/cfwebhook.php',
 	'englishcountries' => '["GB","US", "CA", "AU"]',
 	'customcountries' => array('language' => 'Dutch', 'countries' => '["BE", "AN", "NL", "SR"]'),
