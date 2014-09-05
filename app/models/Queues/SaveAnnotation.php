8f780e0cbd0c6969aa749ea53cdc0c752c4e251a
Oana Inel <oana.inel@gmail.com>
2014-09-05 10:34:47 +0000

    catch exception workerunit

--- app/models/Queues/SaveAnnotation.php
+++ app/models/Queues/SaveAnnotation.php
@@ -7,11 +7,17 @@ class SaveWorkerunit {
 	public function fire($job, $data){
 		// Metrics
 		$workerunit = unserialize($data['workerunit']);
-		$workerunit->save();
-		\Log::debug("Saved workerunit {$workerunit->_id}.");
-
-		Queue::push('Queues\UpdateUnits', [$workerunit->unit_id]);
-
+		try {
+			$workerunit->save();
+			\Log::debug("Saved workerunit {$workerunit->_id}.");
+			Queue::push('Queues\UpdateUnits', [$workerunit->unit_id]);
+		} catch (Exception $e) {
+			// Something went wrong with creating the Entity
+		//	$activity->forceDelete();
+			\Log::debug("Something went wrong with creating the workerunit {$workerunit->_id}.");
+			$workerunit->forceDelete();
+		}
+		
 		$job->delete(); // the Queue job...
 	}
 
