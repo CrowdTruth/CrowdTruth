7241f2cba1d7b6fcd16005fe927e0ca2d2a61590
Bouncer <dev@bouncer.nl>
2014-09-26 16:47:45 +0200

    Config fix

--- app/config/packages/crowdtruth/mturk/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-        'rooturl' => 'https://mechanicalturk.sandbox.amazonaws.com/',
-        'sandboxurl' => 'https://mechanicalturk.sandbox.amazonaws.com/',
-        'accesskey' => '',
-        'secretkey' => ''
-);
-
