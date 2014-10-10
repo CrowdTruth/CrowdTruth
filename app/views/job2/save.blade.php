e048f07e0551a2d51843cf8f2c614712539e2926
lr292358 <lukasz21212121@wp.pl>
2014-10-10 16:43:10 +0000

    form

--- app/views/job2/save.blade.php
+++ app/views/job2/save.blade.php
@@ -8,41 +8,41 @@
 			<div>
 				<div class="panel panel-default">		
 					<div class="panel-heading">
-						<h4>Save the current template (ensure that the job is refreshed)</h4>
+						<h4>Save the current template</h4>
 					</div>
 					<div class="panel-body">
 						{{ Form::open(array('class' => 'form-horizontal jobconf', 'action' => array('JobsController2@postSavet', 'sandbox'), 'method' => 'POST')) }}
-							<fieldset>
+						<fieldset>
 								
-							{{ Form::label('templateType', 'Template type name', 
-									array('class' => 'col-xs-5 control-label')) }}
-									<div class="input-group col-xs-4">		
+							{{ Form::label('templateType', 'New template name:', 
+									array('class' => 'col-xs-4 control-label')) }}
+								<div class="input-group col-xs-3">		
 									{{ Form::text('templateType', Session::get('type_t'), array('class' => 'form-control col-xs-3')) }}
 								</div>
 						
-							{{ Form::label('Overwrite', 'Overwrite?', 
-								array('class' => 'col-xs-5 control-label')) }}
-							<fieldset>
+							{{ Form::label('Allow', 'Overwrite existing template with this one in the Template Library?', 
+								array('class' => 'col-xs-4 control-label')) }}
 							
-							{{ Form::Radio('overwrite','yes', false, array('id' => 'rb_yes')) }}
-							{{ Form::Label('rb_yes', 'yes') }}
-<br/>
 							
-							{{ Form::Radio('overwrite','no',  true, array('id' => 'rb_no')) }}
-							{{ Form::Label('rb_no', 'no') }}
+							{{ Form::Checkbox('overwrite','yes', false, array('id' => 'rb_yes')) }}
+						
+
+							
 						</fieldset>
-							</fieldset>
+	
+					   
 							<br/><br/>
-							{{ Form::label('Load', 'Load this template to the current job?', 
-								array('class' => 'col-xs-5 control-label')) }}
+							{{ Form::label('Load', 'Current job:', 
+								array('class' => 'col-xs-4 control-label')) }}
 							<fieldset>
 							
 							{{ Form::Radio('load','yes', true, array('id' => 'rb_yes')) }}
-							{{ Form::Label('rb_yes', 'yes') }}
+							{{ Form::Label('rb_yes', 'Update it with the above new template') }}
 <br/>
 							
 							{{ Form::Radio('load','no',  false, array('id' => 'rb_no')) }}
-							{{ Form::Label('rb_no', 'no') }}
+							{{ Form::Label('rb_no', 'Keep the old template') }}
+						
 						</fieldset>
 <br/><br/>
 						{{ Form::submit('Save', array('class' => 'btn btn-lg btn-primary pull-right', 'style' => 'margin-right:20px')); }}
--- app/views/media/search/layouts/hb-job.blade.php
+++ app/views/media/search/layouts/hb-job.blade.php
@@ -155,7 +155,7 @@
 			            	<div id="status@{{@index}}" class="currentstatus status-@{{this.status}}">@{{this.status}}</div>
 							<div class="btn-group actionbar">
 								<a class="btn btn-default btn-sm" href="/jobs2/duplicate/@{{this._id}}" data-toggle="tooltip" data-placement="top" title="Duplicate and edit job"><i class="fa fa-files-o"></i></a>
-								<a class="btn btn-success btn-sm" href="/jobs2/refresh/@{{this._id}}" data-toggle="tooltip" data-placement="top" title="Refresh the job settings"><i class="fa fa-refresh"></i></a>
+								<a class="btn btn-default btn-sm" href="/jobs2/refresh/@{{this._id}}" data-toggle="tooltip" data-placement="top" title="Refresh the job settings"><i class="fa fa-refresh"></i></a>
 
 								@{{#if this.url}}
 								    <a class="btn btn-default btn-sm" href="@{{this.url}}" target="_blank" data-toggle="tooltip" data-placement="top" title="Visit task"><i class="fa fa-external-link"></i></a>
@@ -173,15 +173,15 @@
 								@{{/is}}
 								</div>
 								<div class="btn-group actionbar">
-									<a class="btn btn-primary btn-sm" href="/jobs2/save/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Save a template"><i class="fa fa-save"></i></a>
+									<a class="btn btn-default btn-sm" href="/jobs2/save/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Save a template"><i class="fa fa-save"></i></a>
 
 									
 
 									@{{#is this.status 'canceled'}}
-										<a class="btn btn-primary btn-sm" href="/jobs2/load/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Load a template"><i class="fa fa-link"></i></a>
+										<a class="btn btn-default btn-sm" href="/jobs2/load/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Load a template"><i class="fa fa-link"></i></a>
 									@{{/is}}
 									@{{#is this.status 'unordered'}}
-										<a class="btn btn-primary btn-sm" href="/jobs2/load/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Load a template"><i class="fa fa-link"></i></a>
+										<a class="btn btn-default btn-sm" href="/jobs2/load/@{{this.platformJobId}}" data-toggle="tooltip" data-placement="top" title="Load a template"><i class="fa fa-link"></i></a>
 									@{{/is}}
 
 									
