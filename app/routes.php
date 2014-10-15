b14b9346d3b97804c77e277bd227b7214979dd5e
Bouncer <dev@bouncer.nl>
2014-10-15 15:12:56 +0200

    Deleted old pages

--- app/routes.php
+++ app/routes.php
@@ -107,15 +107,8 @@ Route::get('/urlsurls', function()
     return Redirect::to('home');
 });
 
+// define routes
 Route::get('home', 'PagesController@index');
-Route::get('info', 'PagesController@info');
-Route::get('papers', 'PagesController@papers');
-Route::get('presentations', 'PagesController@presentations');
-Route::get('team', 'PagesController@team');
-Route::get('contact', 'ContactController@contact');
-Route::post('contact_request', 'ContactController@contactForm');
-Route::get('api/examples', 'PagesController@apiExamples');
-Route::get('templates/examples', 'PagesController@templatesExamples');
 Route::controller('api/v1', '\Api\v1\apiController');
 Route::controller('api/media', '\Api\media\apiController');
 Route::controller('api/search', '\Api\search\apiController');
