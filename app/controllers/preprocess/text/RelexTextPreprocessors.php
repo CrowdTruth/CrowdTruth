8a0f403e0101aea1e56f22ed3e579f1d94a7eca8
Carlos Martinez <c.martinez@esciencecenter.nl>
2014-10-16 16:34:36 +0200

    Skeleton for relex preprocessing functionality.

---
+++ app/controllers/preprocess/text/RelexTextPreprocessors.php
@@ -0,0 +1,131 @@
+<?php
+namespace Preprocess\Relex;
+
+use Preprocess\AbstractTextPreprocessor as AbstractTextPreprocessor;
+
+// TODO: implement properly and document !
+class RelationInSentencePreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Relation in sentence';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'relationInSentence';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class RelationOutsideTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Relation outside terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'relationOutsideTerms';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class RelationBetweenTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Relation between terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'relationBetweenTerms';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class SemicolonBetweenTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Semicolon between terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'hasSemicolon';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class CommaSeparatedTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Comma separated terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'hasComma';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class ParenthesisAroundTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Parenthesis around terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'hasParenthesis';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
+
+class OverlapingTermsPreprocessor extends AbstractTextPreprocessor {
+	function getName() {
+		return 'Overlapping terms';
+	}
+
+	function getParameterJSFunctionName() {
+		return 'hasOverlappingTerms';
+	}
+
+	function getParameterJSFunction() {
+		return '<script src="/js/preprocessors/text/relextextpreprocessors.js"></script>';
+	}
+
+	function processItem($params, $data, $entities) {
+		return 'IMPLEMENT!!!';
+	}
+}
