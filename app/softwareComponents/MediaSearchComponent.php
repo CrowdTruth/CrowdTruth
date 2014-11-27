2e45c096d6ba204caf2f15f2018b88d2e349b097
Bouncer <dev@bouncer.nl>
2014-11-27 16:05:19 +0100

    Media search component fix

--- app/softwareComponents/MediaSearchComponent.php
+++ app/softwareComponents/MediaSearchComponent.php
@@ -1,18 +1,13 @@
 <?php
 namespace SoftwareComponents;
-
 use \MongoDB\Entity as Entity;
 use \MongoDB\SoftwareComponent as SoftwareComponent;
-
 class MediaSearchComponent {
 	protected $softwareComponent;
 	
 	public function __construct() {
 		$this->softwareComponent = SoftwareComponent::find('mediasearchcomponent');
 	}
-
-<<<<<<< HEAD
-=======
 	public function clear() {
 		$this->softwareComponent['keys'] = [];
 		$this->softwareComponent['keyLabels'] = [];
@@ -22,12 +17,9 @@ class MediaSearchComponent {
 	public function getKeyLabels() {
 		return $this->softwareComponent['keyLabels'];
 	}
-
->>>>>>> ee3c788900d3ad895bac5871f1bc508916b87b43
 	public function store($keys) {
 		$allKeys = $this->softwareComponent['keys'];
 		$allKeys = array_unique(array_merge($allKeys, $keys));
-
 		$labels = $this->softwareComponent['keyLabels'];
 		foreach($keys as $key) {
 			if( ! array_key_exists($key, $labels)) {
@@ -37,10 +29,6 @@ class MediaSearchComponent {
 		}
 		$this->softwareComponent['keys'] = $allKeys;
 		$this->softwareComponent['keyLabels'] = $labels;
-<<<<<<< HEAD
-		
-=======
->>>>>>> ee3c788900d3ad895bac5871f1bc508916b87b43
 		$this->softwareComponent->save();
 	}
 }
