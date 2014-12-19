ba38f2dd0d5e7d440333d518c630794445032664
Bouncer <dev@bouncer.nl>
2014-12-19 17:40:43 +0100

    Fixed profile navigation tab not working

--- app/views/user/nav.blade.php
+++ app/views/user/nav.blade.php
@@ -1,5 +1,5 @@
 <ul class="nav nav-tabs">
-	<li{{ (Request::is('user/*/') ? ' class="active"' : '') }}>{{ link_to('user/' . $user['id'], "Profile") }}</li>
+	<li{{ (Request::is('user/*') && !Request::is('user/*/*') ? ' class="active"' : '') }}>{{ link_to('user/' . $user['id'], "Profile") }}</li>
 	<li{{ (Request::is('user/*/activity') ? ' class="active"' : '') }}>{{ link_to('user/' . $user['id'] . '/activity', "Activity") }}</li>
 	<li{{ (Request::is('user/*/settings') ? ' class="active"' : '') }}>{{ link_to('user/' . $user['id'] . '/settings', "Settings") }}</li>
 	<!-- <li{{ (Request::is('preprocess/textrazor') ? ' class="active"' : '') }}>{{ link_to('preprocess/textrazor', "Textrazor") }}</li> -->
