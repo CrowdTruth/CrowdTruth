45963ac7d90718b4aa0fb48966296f78ace323cd
Bouncer <dev@bouncer.nl>
2015-02-05 15:04:46 +0100

    Merge fixes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -43,18 +43,6 @@ class UserController extends BaseController {
 		return Redirect::to('');
 	}
 	    
-	/**
-     * Display current user activity
-     */
-	public function getActivity() {
-	
-		// redirect if user is not logged in
-		if(!Auth::check())
-			return Redirect::to('/');
-
-		$activities = Activity::getActivitiesForUser(Auth::user()->_id);
-        return View::make('user/activity')->with('activities', $activities);
-    }
 
 	/**
 	 * Display user profile
